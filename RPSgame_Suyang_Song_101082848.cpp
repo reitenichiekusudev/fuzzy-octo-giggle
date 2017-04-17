442af8efdce2502f2a6c021883c625f665c7162c
Andrew <suyangsong@gmail.com>
2017-04-17 18:52:49 -0400

    c++ assignment

--- RPSgame_Suyang_Song_101082848.cpp
+++ RPSgame_Suyang_Song_101082848.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main (void)
+{
+	int choice;
+	bool gamefinished = false;
+	string name;
+	cout<<"please enter your name"<<endl;
+	getline(cin,name);
+while(gamefinished == false)
+{
+	cout<<"current player name: "<<name<<endl;
+	cout<<"1. start the game"<<endl<<"2. Change the player name"<<endl<<"3. show highest scores"<<endl<<"4. exit"<<endl;
+	cin>>choice;
+	cin.ignore();
+		if(choice == 4)
+			{
+				cout<<"thank you for playing!"<<endl;
+				gamefinished = true;
+			}
+}
+}
